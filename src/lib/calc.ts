5d656de1bc4f276bd55b0b6175f12c9aafe98600
Seven <edrezner7@gmail.com>
2025-09-13 00:27:40 -0400

    feat(calc): add hitChance/critChance/expectedDamage with advantage/disadvantage

---
+++ src/lib/calc.ts
@@ -0,0 +1,77 @@
+export function hitChance({
+    attackBonus,
+    targetAC,
+    advantage = false,
+    disadvantage = false,
+} : {
+    attackBonus: number;
+    targetAC: number;
+    advantage?: boolean;
+    disadvantage?: boolean;
+}): number {
+    let hits = 0;
+
+    for (let roll = 1; roll <= 20; roll++) {
+        if (roll === 20) {
+            hits++;
+        } else if (roll === 1) {
+            continue;
+        } else if (roll + attackBonus >= targetAC) {
+            hits++;
+        } else {
+            continue;
+        }
+    }
+    
+    const p = hits / 20;
+    const pAdv = 1 - (1 - p) ** 2;
+    const pDisadv = p ** 2;
+
+    if (advantage && disadvantage) {
+        return p;
+    } else if (advantage) {
+        return pAdv;
+    } else if (disadvantage) {
+        return pDisadv;
+    } else {
+        return p;
+    }
+   
+};
+
+export function critChance({ 
+    critRange = 20, 
+    advantage = false, 
+    disadvantage = false 
+}: { 
+    critRange?: number, 
+    advantage?: boolean, 
+    disadvantage?: boolean 
+}) : number {
+    const cr = Math.min(Math.max(critRange, 2), 20);
+    const p = (21 - cr) / 20;
+    const pAdv = 1 - (1 - p) ** 2;
+    const pDisadv = p ** 2;
+
+    if (advantage && disadvantage) {
+        return p;
+    } else if (advantage) {
+        return pAdv;
+    } else if (disadvantage) {
+        return pDisadv;
+    } else {
+        return p;
+    }
+}
+
+export function expectedDamage({ 
+    hitChance, 
+    critChance, 
+    avgDamage 
+} : {
+    hitChance: number;
+    critChance: number;
+    avgDamage: number;
+}) : number {
+    return avgDamage * (hitChance + critChance);
+}
